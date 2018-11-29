1a2a46b69bef7420c83466825f736a391b0189cd
Sergey Mironchuk <sergey@mironchuk.in.ua>
2018-11-29 21:24:46 +0200

    Version 1.0.0 Templates, Actions, Async content

--- app/app.js
+++ app/app.js
@@ -1,14 +1,34 @@
 'use strict';
 
 // Declare app level module which depends on views, and core components
-angular.module('myApp', ['mspcDataTable',
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-controller('MainCtrl', ["$scope", function MainCtrl($scope){
-  $scope.myEvent = function(id){
-      alert( 'callback ' + id);
-  }
+angular.module('myApp', ['mspDataTable']).
+controller('MainCtrl', ["$scope", "$q", function MainCtrl($scope, $q){
+    $scope.onEmail = function(id){
+        alert( 'EMailed to ' + id);
+    };
+    $scope.onDelete = function(id){
+        alert( 'Deleted ' + id);
+    };
+    $scope.getAsyncContent1 = function(id){
+      return $q(function(resolve, reject) {
+          setTimeout(function() {
+              if (true) {
+                  resolve(id + '-<i class="far fa-smile fa-sm"></i>&nbsp;&nbsp;&nbsp;');
+              } else {
+                  reject('error');
+              }
+          }, 1000);
+      });
+    };
+    $scope.getAsyncContent2 = function(id){
+        return $q(function(resolve, reject) {
+            setTimeout(function() {
+                if (true) {
+                    resolve('City-' + id);
+                } else {
+                    reject('error');
+                }
+            }, 1000);
+        });
+    };
 }]);
---
+++ app/mspDataTables/data-table.directive.js
@@ -0,0 +1,174 @@
+angular
+.module('mspDataTable', [])
+.directive('mspDataTable', function() {
+    return{
+        transclude: true,
+        template: `
+            <table style="width:100%"></table>
+            <div ng-transclude></div>
+            `,
+        scope: {
+            datasourceUrl: "@",
+            className: "@"
+        },
+        link: function(scope, element, attrs, controller){
+            let dtColumns = scope.columns.map(function(c){
+                return {data: c.modelProperty ? c.modelProperty : null };
+            });
+            let dtColumnDefs = [];
+            let columnNumber = 0;
+            scope.columns.forEach(function (column){
+                let columnDef = {
+                    "targets": columnNumber,
+                    "title": column.header,
+                    "sortable": eval(column.sortable)
+                };
+                if (column.className) {
+                    columnDef.className = column.className;
+                }
+                let asyncContents = "";
+                if (column.asyncContents.length > 0) {
+                    let asyncContentNumber = 0;
+                    column.asyncContents.forEach(function(asyncAction){
+                        asyncContents += "<span async-content-id='"
+                            + columnNumber + "_" + asyncContentNumber++ + "'>"
+                            + asyncAction.spinTemplate + "</span>";
+                    });
+                }
+                let actions = "";
+                if (column.actions.length > 0) {
+                    let actionNumber = 0;
+                    column.actions.forEach(function(action){
+                        if (action.template) {
+                            let html = $($.parseHTML(action.template));
+                            html.first().attr("action-id", columnNumber + "_" + actionNumber++);
+                            html.css('cursor', 'pointer');
+                            actions += html.prop('outerHTML');
+                        }
+                    });
+                }
+                if (column.template) {
+                    columnDef.render = function(data, type, model) {
+                        return eval(column.template) + " " + asyncContents + " " + actions;
+                    }
+                }
+                else if ((actions || asyncContents) && column.modelProperty){
+                    columnDef.render = function(data, type, model) {
+                        return data + ' ' + asyncContents + ' ' + actions;
+                    }
+                }
+                else if (actions || asyncContents){
+                    columnDef.render = function(data, type, model) {
+                        return asyncContents + ' ' + actions;
+                    }
+                }
+                dtColumnDefs.push(columnDef);
+                columnNumber++;
+            });
+
+            let tableElement = element.find('table').first();
+            tableElement.addClass(scope.className);
+            tableElement.DataTable( {
+                "ajax": scope.datasourceUrl,
+                "columns": dtColumns,
+                "columnDefs": dtColumnDefs
+
+            });
+
+            let table = tableElement.DataTable();
+            tableElement.on('click', '[action-id]', function () {
+                let actionElement = $(this);
+                let tr = actionElement.parents("tr");
+                let model = table.row( tr ).data();
+                let actionId = actionElement.attr("action-id");
+                if (actionId) {
+                    let actionIdArray = actionId.split('_');
+                    let action = scope.columns[+actionIdArray[0]].actions[+actionIdArray[1]];
+                    action.onAction({id: model.id});
+                }
+            } );
+            table.on('draw', function () {
+                tableElement.find('[async-content-id]').each(function(){
+                    let contentElement = $(this);
+                    let tr = contentElement.parents("tr");
+                    let model = table.row( tr ).data();
+                    let contentId = contentElement.attr("async-content-id");
+                    if (contentId) {
+                        let contentIdArray = contentId.split('_');
+                        let asyncContent = scope.columns[+contentIdArray[0]].asyncContents[+contentIdArray[1]];
+                        asyncContent.getAsyncContent({id: model.id})
+                            .then(function(content){
+                                contentElement.html(content);
+                            })
+                            .catch(function(error){
+                                contentElement.html(error);
+                            });
+                    }
+                });
+            } );
+        },
+        controller: ["$scope", function ($scope) {
+            this.columns = $scope.columns = [];
+            this.addColumn = function(scope){
+                this.columns.push(scope);
+            };
+        }]
+    }
+})
+.directive('mspColumn', function() {
+    return {
+        transclude: true,
+        template: '<div ng-transclude></div>',
+        require: '^^mspDataTable',
+        restrict: 'E',
+        scope: {
+            modelProperty: '@',
+            header: '@',
+            template: "@",
+            className: "@",
+            sortable: "@"
+        },
+        link: function(scope, element, attrs, tableCtrl) {
+            tableCtrl.addColumn(scope);
+        },
+        controller: ["$scope", function ($scope) {
+            this.actions = $scope.actions = [];
+            this.asyncContents = $scope.asyncContents = [];
+            this.addAction = function(action){
+                this.actions.push(action);
+            };
+            this.addAsyncContent = function(asyncContent){
+                this.asyncContents.push(asyncContent);
+            };
+        }]
+    }
+})
+.directive('mspColumnAction', function() {
+    return {
+        transclude: true,
+        require: '^^mspColumn',
+        restrict: 'E',
+        scope: {
+            onAction: "&",
+            template: '@'
+        },
+        link: function(scope, element, attrs, columnCtrl) {
+            scope.template = "<span>" + scope.template + "&nbsp;</span>";
+            columnCtrl.addAction(scope);
+        }
+    }
+})
+.directive('mspAsyncContent', function() {
+    return {
+        transclude: true,
+        require: '^^mspColumn',
+        restrict: 'E',
+        scope: {
+            getAsyncContent: "&",
+            spinTemplate: '@'
+        },
+        link: function(scope, element, attrs, columnCtrl) {
+            columnCtrl.addAsyncContent(scope);
+        }
+    }
+});
